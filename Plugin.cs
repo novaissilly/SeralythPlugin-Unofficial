27ff9c0f884a5fa95d540f682fc8c201f556f679
Novaafr <idk777800111@gmail.com>
2026-06-30 03:05:11 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,82 @@
+﻿using static Seralyth.Menu.Main;
+using Seralyth.Classes.Menu;
+using Seralyth.Mods;
+using UnityEngine;
+using Seralyth.Managers;
+using Seralyth.Menu;
+
+namespace SeralythPlugin
+{
+    public class Plugin
+    {
+        /*
+            Plugin template used for Seralyth Menu 1.0.0+
+
+            If you've ever created a menu before, this should be easy:
+                + GetIndex(ButtonName); Gets a button's ButtonInfo
+                + AddButton(CategoryID, new ButtonInfo); Creates a button in a category
+                - RemoveButton(CategoryID, ButtonName); Removes a button in a category
+
+                + GetCategory(CategoryName); Gets a category, you can find the category names in the menu's Buttons.cs file at the bottom
+                + AddCategory(CategoryName); Creates a category, does not automatically create the navigation button
+                - RemoveCategory(CategoryName); Removes the category created, does not automatically
+
+            Feel free to modify, add, or remove any methods or fields below
+        */
+
+        public static string Name = "Example Plugin";
+        public static string Description = "An example plugin used for testing.";
+
+        // This runs when the plugin starts, at the beginning of the game if enabled before launch
+        // You should put all of your adding of buttons / categories here
+        public static void OnEnable()
+        {
+            LogManager.Log("Plugin " + Name + " has been enabled!");
+
+            int category = Buttons.AddCategory("Plugin Mods");
+            Buttons.AddButton(Buttons.GetCategory("Main"), new ButtonInfo { buttonText = "Plugin Mods", method =() => Buttons.CurrentCategoryIndex = category, isTogglable = false, toolTip = "Brings you to a category for plugins." });
+
+            Buttons.AddButtons(
+                category,
+                new ButtonInfo[]
+                {
+                    new ButtonInfo { buttonText = "Exit Plugin Mods", method =() => Buttons.CurrentCategoryName = "Main", isTogglable = false, toolTip = "Returns you back to the main page." },
+                    new ButtonInfo { buttonText = "Right Trigger Fly <color=grey>[</color><color=green>RT</color><color=grey>]</color>", method = () => RightTriggerFly(), toolTip = "Returns you back to the main page." }
+                }
+            );
+        }
+
+        // This runs when the plugin stops, or when plugins are reloaded
+        // You should put all of your removing of buttons / categories here
+        public static void OnDisable()
+        {
+            LogManager.Log("Plugin " + Name + " has been disabled!");
+
+            Buttons.RemoveCategory("Plugin Mods");
+            Buttons.RemoveButton(Buttons.GetCategory("Main"), "Plugin Mods");
+        }
+
+        // This runs every frame before the mods
+        public static void Update()
+        {
+            // LogManager.Log(Time.time);
+        }
+
+        // This runs every frame when the menu UI is open (togglable with backslash)
+        // Don't use this like Update
+        public static void OnGUI()
+        {
+            // GUI.Button(new Rect(10, 10, 200, 100), "Test Button");
+        }
+
+        // Example mod
+        private static void RightTriggerFly()
+        {
+            if (rightTrigger > 0.5f)
+            {
+                GorillaLocomotion.GTPlayer.Instance.transform.position += GorillaTagger.Instance.headCollider.transform.forward * Time.deltaTime * Movement.FlySpeed;
+                GorillaLocomotion.GTPlayer.Instance.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            }
+        }
+    }
+}
